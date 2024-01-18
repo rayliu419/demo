1a3bdd58f8848434ace8015e59034d34399544df
rayliu419 <lenny419@163.com>
2024-01-18 21:39:11 +0800

    use docker-compose to setup dependency middleware, E2E from app to prometheus

--- docker/MetaSql.sql
+++ docker/MetaSql.sql
@@ -1,15 +1,10 @@
--- 创建元数据的SQL语句
 
-drop table user;
-drop table department;
-
--- 创建department表
 create table department (
    id int AUTO_INCREMENT primary key not null,
    name varchar(128) null
 );
 
--- 创建user表
+
 create table user (
    id varchar(128) primary key not null,
    name varchar(128) null,
--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -12,6 +12,7 @@ import com.example.service.UserService;
 import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +30,7 @@ public class UserController {
     @Autowired
     MeterRegistry meterRegistry;
 
+    @Timed(value = "user create")
     @PostMapping("/create")
     public String create(@RequestBody CreateUserRequestDTO createUserRequest) {
         log.info("In UserController create");
@@ -37,6 +39,7 @@ public class UserController {
         return "create user";
     }
 
+    @Timed(value = "user get")
     @GetMapping("/get")
     @ResponseBody
     public GetUserByNameResponseDTO getByName(@RequestParam(value = "name") String name) {
@@ -49,6 +52,7 @@ public class UserController {
         return getUserByNameResponseDTO;
     }
 
+    @Timed(value = "user getAll")
     @GetMapping("/getAll")
     @ResponseBody
     public GetAllByNameResponseDTO getAllByName(@RequestParam(value = "name") String name) {
@@ -60,14 +64,32 @@ public class UserController {
         return getAllByNameResponseDTO;
     }
 
+    @Timed(value = "test Timer")
+    @GetMapping("/timerTest")
+    @ResponseBody
+    public void timerTest() {
+        Timer timer = Timer
+                .builder("test.timer")
+                .description("a description of what this timer does") // optional
+                .tags("region", "test") // optional
+                .register(meterRegistry);
+        timer.record(() -> {log.info("hit timerTest");});
+    }
+
     /**
      * 演示Counter变量打印出来的metrics
-     * output:
+     * 在日志中的output:
      *    get.user.by.name{name=ivy} throughput=0.016667/s
      * 看起来跟想象的不一样，不一样打印 1 才对吗？为什么会有throughput?
+     *
+     * 在http://localhost:8080/actuator/prometheus打印的
+     * # HELP get_user_by_name_total indicates get user by name api call count
+     * # TYPE get_user_by_name_total counter
+     * get_user_by_name_total{name="ivy",} 1.0
      * @param name
      */
     private void incGetUserByNameCount(String name) {
+        // 这里的Counter的初始化是不是应该在单独的构造函数中，在这里只是调用counter.increment(1.0)？
         Counter counter = Counter
                 .builder("get.user.by.name")
                 .description("indicates get user by name api call count")
@@ -75,5 +97,7 @@ public class UserController {
                 .register(meterRegistry);
         counter.increment(1.0);
     }
+
+
 
 }
